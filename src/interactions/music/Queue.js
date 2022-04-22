1da01b606e44e6c716892dc749ad199bb70d64ab
HerrCraziDev <herrcrazi@gmail.com>
2022-04-23 01:22:31 +0200

    Longer queue lists

--- src/interactions/music/Queue.js
+++ src/interactions/music/Queue.js
@@ -3,6 +3,7 @@ const KongouInteraction = require('../../abstract/KongouInteraction.js');
 const KongouDispatcher = require('../../modules/KongouDispatcher.js');
 const { MessageEmbed } = require('discord.js');
 
+const maxItems = 20;
 class Queue extends KongouInteraction {
     get name() {
         return 'queue';
@@ -17,13 +18,13 @@ class Queue extends KongouInteraction {
     }
 
     async run({ interaction, dispatcher }) {
-        const queue = dispatcher.queue.length > 9 ? dispatcher.queue.slice(0, 9) : dispatcher.queue;
+        const queue = dispatcher.queue.length > maxItems ? dispatcher.queue.slice(0, maxItems) : dispatcher.queue;
         const embed = new MessageEmbed()
             .setColor(this.client.color)
             .setTitle('▶️ Now Playing')
             .setThumbnail(`https://img.youtube.com/vi/${dispatcher.current.info.identifier}/default.jpg`)
             .setDescription(`[${dispatcher.current.info.title}](${dispatcher.current.info.uri}) [${KongouDispatcher.humanizeTime(dispatcher.current.info.length)}]`)
-            .setFooter(`• ${dispatcher.queue.length} total songs in queue`);
+            .setFooter(`${this.client.user.username} • ${dispatcher.queue.length} total songs in queue`, this.client.user.displayAvatarURL());
         if (queue.length) embed.addField('⏩ Up Next', queue.map((track, index) => `**${index + 1} 〉** __${track.info.title}__ • ${track.info.author}`).join('\n'));
         await interaction.reply({ embeds: [ embed ] });
     }
